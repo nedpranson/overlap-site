8bbfae96950916fee792109300493615d7cc7f8f
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2026-01-25 22:45:38 +0200

    Add seo tags and metadata with sitemaps and robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: "https://overlap.gg",
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [sitemap({
+        changefreq: 'weekly',
+        priority: 1.0,
+        lastmod: new Date(),
+    })]
 });
